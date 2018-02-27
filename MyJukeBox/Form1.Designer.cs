b5efc74289a9dc0680564735a387656ebfe0ed0c
RowanCattermoul <34025986+RowanCattermoul@users.noreply.github.com>
2018-02-27 16:05:10 +0000

    test

--- MyJukeBox/Form1.Designer.cs
+++ MyJukeBox/Form1.Designer.cs
@@ -34,9 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 482);
+            this.ClientSize = new System.Drawing.Size(416, 482);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "My Juke Box";
             this.ResumeLayout(false);
 
         }
